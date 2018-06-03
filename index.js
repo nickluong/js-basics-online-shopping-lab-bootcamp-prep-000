58d49b6fdb9b8f5594f1fd6602dbcdb30f8dc259
Nicholas  Luong <nckluong@gmail.com>
2018-06-03 02:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,5 +61,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  
+  if(!cardNumber)
+  return 'Sorry, we don\'t have a credit card on file for you.'
+  else{
+    for(let i = 0 ; i < cart.length ; i++){
+      cart.unshift()
+    }
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  }
 }
